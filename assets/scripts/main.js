34edfc555ced187e6d8a9f67f9df9e0378464bbb
d7hernan <83631798+d7hernan@users.noreply.github.com>
2022-11-13 10:57:55 +0000

    literally wrote 1 line im calling it a day

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,10 +68,13 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  const str_items = JSON.parse(localStorage.getItem('recipes'));
+  if (str_items != null) return str_items;  
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  const array = []; 
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
